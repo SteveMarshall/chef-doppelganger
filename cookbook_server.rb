33b6ecfe2331976165f1e58ccb7ded9da1f4b499
Steve Marshall <steve@nascentguruism.com>
2014-01-06 21:30:05 +0000

    Test for valid JSON.

--- cookbook_server.rb
+++ cookbook_server.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
+
+require 'sinatra'
+
+get '/cookbooks' do
+  content_type :json
+  "{}"
+end
--- spec/cookbook_server_spec.rb
+++ spec/cookbook_server_spec.rb
@@ -1,5 +1,21 @@
 # spec/app_spec.rb
+require 'json'
+
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "Cookbook Server" do
+shared_examples 'json' do
+  it 'should return a JSON Content-Type header' do
+    get subject
+    last_response.headers['Content-Type'].should match(%r{application/json\b})
+  end
+
+  it 'should return valid JSON' do
+    get subject
+    expect { JSON.parse last_response.body }.not_to raise_error
+  end
+end
+
+describe 'Cookbook list' do
+  subject { '/cookbooks' }
+  it_should_behave_like 'json'
 end
